0a442bb78f17c51ef49169e28a8da3f02ce7b981
Stijn <stijnvcuijk@gmail.com>
2020-03-13 12:33:17 +0100

    Update

--- unprefixed/jquery.subtle-slideshow.js
+++ unprefixed/jquery.subtle-slideshow.js
@@ -13,8 +13,9 @@ $.fn.slideshow = function(options){
     animate: true,
     pauseOnTabBlur: true,
     enableLog: false,
-    slideElementClass: 'slide', // This is also defined in the CSS!
-    slideshowId: 'slideshow' // This is also defined in the CSS!
+    slideElementClass: 'slide',  // This is also defined in the CSS!
+    slideshowElementClass: 'slideshow',  // This is also defined in the CSS!
+    slideshowId: 'slideshow'
   }, options);
 
   // Turn off console log messages
@@ -31,7 +32,7 @@ $.fn.slideshow = function(options){
   }
 
   // Insert slideshow element
-  $('<div id="' + settings.slideshowId + '"></div>').insertBefore(slides);
+  $('<div class="' + settings.slideshowElementClass + '" id="' + settings.slideshowId + '"></div>').insertBefore(slides);
 
   // Define variables
   var paused = false;
@@ -39,14 +40,14 @@ $.fn.slideshow = function(options){
   var slideTimeDelta = 0;
   var resumeStartTime = 0;
   var resumeTimer;
-  if(settings.animate == true){
+  if (settings.animate == true) {
     var cssAnimationDuration = settings.slideDuration + settings.fadeDuration;
-  }else{
+  } else {
     slides.find('.' + settings.slideElementClass + ' span.animate').removeClass('animate');
     var cssAnimationDuration = 0;
   }
 
-  console.log('Slideshow initialized.');
+  console.log(settings.slideshowId + ': Slideshow initialized.');
 
   // Add the first slide to the slideshow
   slides.find('.' + settings.slideElementClass + ':first span.animate').addClass('active').css('animation-duration', cssAnimationDuration + 'ms')
@@ -54,47 +55,64 @@ $.fn.slideshow = function(options){
   var currentSlideStartTime = Date.now();
 
   // Start interval loop
-  slidesInterval = setInterval(slideRefresh, settings.slideDuration);
+  var slidesInterval = setInterval(slideRefresh, settings.slideDuration);
 
-  console.log('Slideshow started.');
+  console.log(settings.slideshowId + ': Slideshow started.');
 
   // setInterval and setTimeout behave weirdly when the tab is out of focus.
   // To prevent glitches and save resources we can pause the slideshow when the
   // tab is out of focus.
   if(settings.pauseOnTabBlur == true){
     $(window).focus(function() {
-      console.log('Window gained focus.');
-      if (paused == true) {
-        console.log('Resuming slideshow.');
-        resumeStartTime = Date.now();
-        paused = false;
-        $('#' + settings.slideshowId + ' span.active:last').removeClass('paused');
-        resumeTimer = setTimeout(function(){
-          slideTimeDelta = 0;
-          slideRefresh();
-          slidesInterval = setInterval(slideRefresh, settings.slideDuration);
-        }, settings.slideDuration - slideTimeDelta);
-      }
+      console.log(settings.slideshowId + ': Window gained focus.');
+      resume();
     }).blur(function() {
+      console.log(settings.slideshowId + ': Window lost focus.');
+      pause();
+    });
+  }
+
+  // Functions
+
+  function pause()  {
+    if (paused == false) {
+      console.log(settings.slideshowId + ': pause(): Slideshow paused.');
       paused = true;
-      console.log('Window lost focus, slideshow paused.');
       if(slideTimeDelta != 0){
         var timeSinceLastPause = Date.now() - resumeStartTime;
         slideTimeDelta = slideTimeDelta + timeSinceLastPause;
-        console.log('Time since last pause within this slide: ' + timeSinceLastPause + ' ms');
+        console.log(settings.slideshowId + ': pause(): Time since last pause within this slide: ' + timeSinceLastPause + ' ms');
       }else{
         slideTimeDelta = Date.now() - currentSlideStartTime;
       }
-      console.log('Current slide at ' + slideTimeDelta + ' ms.');
+      console.log(settings.slideshowId + ': pause(): Current slide at ' + slideTimeDelta + ' ms.');
       $('#' + settings.slideshowId + ' span.active:first').addClass('paused');
       clearInterval(slidesInterval);
       clearTimeout(resumeTimer);
-    });
+    } else {
+      console.log(settings.slideshowId + ': pause(): Slideshow was already paused.');
+    }
+  }
+
+  function resume() {
+    if (paused == true) {
+      console.log(settings.slideshowId + ': resume(): Resuming slideshow.');
+      resumeStartTime = Date.now();
+      paused = false;
+      $('#' + settings.slideshowId + ' span.active:last').removeClass('paused');
+      resumeTimer = setTimeout(function(){
+        slideTimeDelta = 0;
+        slideRefresh();
+        slidesInterval = setInterval(slideRefresh, settings.slideDuration);
+      }, settings.slideDuration - slideTimeDelta);
+    } else {
+      console.log(settings.slideshowId + ': resume(): Slideshow was not paused.');
+    }
   }
 
   // Prepends a new slide to the slideshow element and fades out the previous one
   function slideRefresh() {
-    console.log('Slide refresh triggered.');
+    console.log(settings.slideshowId + ': slideRefresh(): Slide refresh triggered.');
     currentSlideStartTime = Date.now();
     var slideshowDOM = slideshow[0];
     // If setInterval glitches out the slideshow will sometimes have no slides in
@@ -102,7 +120,7 @@ $.fn.slideshow = function(options){
     // if it is empty. Pausing the slideshow on tab blur will prevent glitches most of the time,
     // so this is a fallback for if that fails.
     if(slideshowDOM.children.length == 0) {
-      console.log('There are no slides in the slideshow.');
+      console.log(settings.slideshowId + ': slideRefresh(): There are no slides in the slideshow.');
       slides.find('.' + settings.slideElementClass + ':first').prependTo(slideshow);
     }else{
       slides.find('.' + settings.slideElementClass + ':first').prependTo(slideshow);
